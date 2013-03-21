10e7fe2853f9dbe3005c905c1ef1378506d53e89
Gregor Brett <gregor.brett@gmail.com>
2013-03-21 15:45:23 +0000

    Initial Commit

---
+++ HypnoTime/HypnoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnoAppDelegate.h
+//  HypnoTime
+//
+//  Created by Gregor Brett on 21/03/2013.
+//  Copyright (c) 2013 Gregor Brett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HypnoTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HypnoTime
+//
+//  Created by Gregor Brett on 21/03/2013.
+//  Copyright (c) 2013 Gregor Brett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HypnoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HypnoAppDelegate class]));
+    }
+}
